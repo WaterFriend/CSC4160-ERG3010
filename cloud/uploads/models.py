365027fdba9b461fe36897375503495a699ae224
WaterFriend <116010169@link,cuhk.edu.cn>
2019-11-28 00:50:00 +0800

    1

--- cloud/uploads/models.py
+++ cloud/uploads/models.py
@@ -5,13 +5,17 @@ import uuid
 
 class Patient(models.Model):
     pID         = models.AutoField(primary_key = True)
-    pFName      = models.CharField(max_length = 20)
-    pLName      = models.CharField(max_length = 20)
+    pFName      = models.CharField(max_length = 30)
+    pLName      = models.CharField(max_length = 30)
     pGender     = models.CharField(max_length = 10)
     pRace       = models.CharField(max_length = 20)
     pEthnicity  = models.CharField(max_length = 20)
+    pStatus     = models.CharField(max_length = 5)
+    pRemark     = models.CharField(max_length = 255)
     pAge        = models.IntegerField()
+    dID         = models.ForeignKey('Doctor', on_delete = models.CASCADE)
 
     def __str__(self):
         return self.pID
 
+class Doctor
