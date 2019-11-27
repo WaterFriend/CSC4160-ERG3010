b5e89072b9a7c7298d176f23106668fbfa8eb03e
WaterFriend <116010169@link,cuhk.edu.cn>
2019-11-28 01:27:19 +0800

    building up the backend of upload page

--- cloud/uploads/views.py
+++ cloud/uploads/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts               import render
-from django.http                    import HttpResponse
-from django.contrib.auth.decorators import login_required
-from django.views.generic.edit      import CreateView
-from django.urls                    import reverse_lazy
+#-*- conding:utf-8 -*-
+import time
+from django.contrib              import auth
+from django.contrib.auth.hashers import make_password,check_password
+from django.contrib.auth.models  import User
+from django.http                 import HttpResponseRedirect, HttpResponse
+from django.shortcuts            import render, redirect
+from django.contrib              import messages
+
+# Create your views here.
+from .models        import Patient
+from login.models   import Doctor
+# from .forms import InfoForm
+
 
