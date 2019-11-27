4d2805dc837b2907eb3703847e90a13ba93db555
WaterFriend <116010169@link,cuhk.edu.cn>
2019-11-28 01:08:47 +0800

    1

--- cloud/uploads/models.py
+++ cloud/uploads/models.py
@@ -5,17 +5,24 @@ import uuid
 
 class Patient(models.Model):
     pID         = models.AutoField(primary_key = True)
-    pFName      = models.CharField(max_length = 30)
-    pLName      = models.CharField(max_length = 30)
-    pGender     = models.CharField(max_length = 10)
-    pRace       = models.CharField(max_length = 20)
-    pEthnicity  = models.CharField(max_length = 20)
-    pStatus     = models.CharField(max_length = 5)
-    pRemark     = models.CharField(max_length = 255)
+    pFName      = models.CharField(max_length = 30,  null=False)
+    pLName      = models.CharField(max_length = 30,  null=False)
+    pGender     = models.CharField(max_length = 10,  null=False)
+    pRace       = models.CharField(max_length = 20,  null=False)
+    pEthnicity  = models.CharField(max_length = 20,  null=True)
+    pStatus     = models.CharField(max_length = 5,   null=False)
+    pRemark     = models.CharField(max_length = 255, null=True)
     pAge        = models.IntegerField()
     dID         = models.ForeignKey('Doctor', on_delete = models.CASCADE)
 
     def __str__(self):
         return self.pID
 
-class Doctor
+
+class Doctor(models.Model):
+    dID         = models.AutoField(primary_key = True)
+    dAccount    = models.CharField(max_length = 15)
+    dPassword   = models.CharField(max_length = 255) 
+
+    def __str__(self):
+        return self.dID
