c42688710337b76cd3f7eb7cb0e20ac9b2249bcb
WaterFriend <116010169@link,cuhk.edu.cn>
2019-11-28 11:06:53 +0800

    1

--- cloud/uploads/views.py
+++ cloud/uploads/views.py
@@ -24,6 +24,7 @@ def upload(request):
         agree       = request.POST.get('age')
 
 
+
         userEmail = request.POST.get('userEmail')
         password = str(request.POST.get('password'))
         repPassword = str(request.POST.get('reppassword'))
