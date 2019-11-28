1a2c2ac69ca30e892302c69ac878ca9c27be1015
WaterFriend <116010169@link,cuhk.edu.cn>
2019-11-28 10:53:56 +0800

    building up upload page

--- cloud/uploads/views.py
+++ cloud/uploads/views.py
@@ -13,3 +13,50 @@ from login.models   import Doctor
 # from .forms import InfoForm
 
 
+def upload(request):
+    if request.method == "POST":
+        check_box = request.POST.getlist('check_box')
+        accountType = request.POST.get('accountType')
+        userEmail = request.POST.get('userEmail')
+        password = str(request.POST.get('password'))
+        repPassword = str(request.POST.get('reppassword'))
+        message = "You should agree with the privacy of Exam and Application System!"
+        if check_box:
+            if accountType != "I am" and accountType != None:           
+                if userEmail != "" and password != "" and repPassword != "":                    #check if user doesn't complete his/her form
+                    if "@" in userEmail:                                                        #check if this is a formal E-mail formate
+                        if RegInfo.objects.filter(reg_id=userEmail).exists() == False:          #check if the account had existed
+                            if password == repPassword:                                         #check if the two passwords are differents 
+
+                                if accountType == "Student":
+                                    sid = "0" + time.strftime("%Y%m%d%H%M%S", time.localtime()) 
+                                    #print(sid)
+                                    Reg = RegInfo.objects.create(reg_id=userEmail, reg_password=password)
+                                    Stu = Student.objects.create(stu_id=sid, stu_email=userEmail, reg=Reg)
+                                    return redirect('../stu/editor/%s' %sid)
+                                
+                                elif accountType == "University":
+                                    uid = "1" + time.strftime("%Y%m%d%H%M%S", time.localtime()) 
+                                    #print(uid)
+                                    Reg = RegInfo.objects.create(reg_id=userEmail, reg_password=password)
+                                    Uni = University.objects.create(uni_id=uid, uni_email=userEmail, reg=Reg)
+                                    return redirect('../uni/editorCreate/%s' %userEmail)  
+
+                                elif accountType == "Guardian":
+                                    gid = "2" + time.strftime("%Y%m%d%H%M%S", time.localtime())
+                                    #print(gid)
+                                    Reg = RegInfo.objects.create(reg_id=userEmail, reg_password=password)
+                                    Gua = Guardian.objects.create(guardian_id=gid, guardian_email=userEmail, reg=Reg) 
+                                    return redirect('../guardian/editor/%s' %gid)                                                      
+                            else:
+                                message = "Your second password is not match, please try again."
+                        else:
+                            message = "The account is already exist!"
+                    else:
+                        message = "Please enter correct E-mail format."
+                else:
+                    message = "Please complete the forms of Email and password!"
+            else:
+                message = "Please select your account type!"
+        return render(request, 'reg/register.html', {"message": message})
+    return render(request, 'reg/register.html')
