239f830c54572a263ec829212668fb553770bea5
WaterFriend <116010169@link.cuhk.edu.cn>
2019-11-29 19:01:33 +0800

    modify home pages

--- cloud/home/urls.py
+++ cloud/home/urls.py
@@ -4,6 +4,6 @@ from . import views
 #from .views import SearchResultsView
 
 urlpatterns = [
-    path('<str:userID>', views.result_list, name = 'result_list'),
+    path('base/<str:doctorID>', views.result_list, name = 'result_list'),
     #path('search/', SearchResultsView.as_view(), name='search_result'),
 ]
--- cloud/home/views.py
+++ cloud/home/views.py
@@ -4,8 +4,11 @@ from django.core.files.storage      import FileSystemStorage
 from django.contrib.staticfiles     import finders
 from django.db.models               import Q
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf   import csrf_exempt
 
 
+
+@csrf_exempt
 def result_list(request, doctorID):
     content = []
-    return render(request, 'base.html', content)
+    return render(request, 'base.html', content)      
