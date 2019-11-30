a527c589e4e2e046c13557accb8788548a792f57
WaterFriend <116010169@link.cuhk.edu.cn>
2019-11-30 19:22:57 +0800

    modify home.views.py

--- cloud/home/views.py
+++ cloud/home/views.py
@@ -1,3 +1,4 @@
+import time
 from django.shortcuts               import render, redirect, render_to_response
 from django.views.generic           import TemplateView, FormView, ListView
 from django.core.files.storage      import FileSystemStorage
@@ -7,19 +8,23 @@ from django.contrib                 import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models               import Q
 from django.views.decorators.csrf   import csrf_exempt
-import json
-import time
-import os
 
+from upload.models      import Patient
 
 
 @csrf_exempt
 def result_list(request, doctorID):
     if request.method == "GET":
+        patientList = Patient.objects.all()
+
+
         content = {
             'uploadLink'  : "../upload/" + doctorID,
+            'patient'     : patientList.filter(dID=doctorID)
         }
         return render(request, 'home.html', content)  
+    
+    
     elif request.method == "POST":   
         logout(request) 
         return redirect('../login/login')
