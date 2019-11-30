20e2f6053c8126d981bbcadb79d893a158ee8794
Isak-L <35722189+Isak-L@users.noreply.github.com>
2019-11-30 17:27:50 +0800

    no need to input patient ID

--- cloud/upload/views.py
+++ cloud/upload/views.py
@@ -23,8 +23,6 @@ def uploadPage(request, doctorID):
 @csrf_exempt
 def upload(request,doctorID):  #(request, userID)
     if request.method == "POST":
-        check_box   = request.POST.getlist('check_box')
-        patientID   = request.POST.get('patientID')
         fName       = request.POST.get('firstName')
         lName       = request.POST.get('lastName')
         gender      = request.POST.get('gender')
@@ -36,34 +34,33 @@ def upload(request,doctorID):  #(request, userID)
         imgURL      = request.POST.get('imgURL')
         
         message = "You should agree with the upload privacy of Cancer Dection System!"
-        if check_box:
-            if fName != "" and lName != "":                                                         #check if user doesn't provide patient's full name
-                if Patient.objects.filter(Q(pFName=fName) | Q(pLName=lName)).exists() == False:     #check if the patient had existed  
-                    if gender != "":                                                                #check if user doesn't provide patient's gender 
-                        if race != "":                                                              #check if user doesn't provide patient's race
-                            if ethnicity != "":                                                     #check if user doesn't provide patient's ethnicity
-                                if status != "":                                                    #check if user doesn't provide patient's status
-                                    if age != None:                                                 #check if user doesn't provide patient's age
-                                        if accountType == "Student":
-                                            patientID = "p" + time.strftime("%Y%m%d%H%M%S", time.localtime()) 
-                                            #print(sid)
-                                            patient = Patient.objects.create(pID=patientID, pFName=fName, pLName=lName, pGender=gender,
-                                                                             pRace=race, pEthnicity=ethnicity, pStatus=status, pAge=age, pRemark=remark, pImage=imgURL, dID=doctorID)                                            
-                                            #return redirect('../stu/editor/%s' %sid)
-                                    else:
-                                        message = "Please provide patient's age!"                                                   
+        if fName != "" and lName != "":                                                         #check if user doesn't provide patient's full name
+            if Patient.objects.filter(Q(pFName=fName) | Q(pLName=lName)).exists() == False:     #check if the patient had existed  
+                if gender != "":                                                                #check if user doesn't provide patient's gender 
+                    if race != "":                                                              #check if user doesn't provide patient's race
+                        if ethnicity != "":                                                     #check if user doesn't provide patient's ethnicity
+                            if status != "":                                                    #check if user doesn't provide patient's status
+                                if age != None:                                                 #check if user doesn't provide patient's age
+                                    if accountType == "Student":
+                                        patientID = "p" + time.strftime("%Y%m%d%H%M%S", time.localtime()) 
+                                        #print(sid)
+                                        patient = Patient.objects.create(pID=patientID, pFName=fName, pLName=lName, pGender=gender,
+                                                                            pRace=race, pEthnicity=ethnicity, pStatus=status, pAge=age, pRemark=remark, pImage=imgURL, dID=doctorID)                                            
+                                        #return redirect('../stu/editor/%s' %sid)
                                 else:
-                                    message = "Please provide patient's status!"
+                                    message = "Please provide patient's age!"                                                   
                             else:
-                                message = "Please provide patient's ethnicity!"
+                                message = "Please provide patient's status!"
                         else:
-                            message = "Please provide patient's race!"
+                            message = "Please provide patient's ethnicity!"
                     else:
-                        message = "Please provide patient's gender!"
+                        message = "Please provide patient's race!"
                 else:
-                    message = "The patient had already exist!"
+                    message = "Please provide patient's gender!"
             else:
-                message = "Please provide patient's full name!"
-        return render(request, 'upload/upload.html', {"message": message})
+                message = "The patient had already exist!"
+        else:
+            message = "Please provide patient's full name!"
+    
     return render(request, 'upload/upload.html')
 
